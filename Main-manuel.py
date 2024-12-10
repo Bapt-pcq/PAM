5b521c57196eab9cd813801a3c3ec0cff25bcbd3
ThomasTravers35 <thomas.travers@ecam-rennes.com>
2024-12-10 10:45:58 +0100

    j'ai ajouter un s à un com

--- Main-manuel.py
+++ Main-manuel.py
@@ -1,5 +1,5 @@
 from first_page import first_page
 
-# Démarrer l'application en appelant first_page()
+# Démarrer l'application en appelants first_page()
 if __name__ == "__main__":
     first_page()
